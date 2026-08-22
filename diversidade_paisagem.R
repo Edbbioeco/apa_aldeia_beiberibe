a1131e715bd08a5340d92296600441cc13e1e756
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-22 13:19:21 -0300

    Função para baixar os rasters

--- diversidade_paisagem.R
+++ diversidade_paisagem.R
@@ -26,3 +26,38 @@ apa
 
 ggplot() +
   geom_sf(data = apa, color = "black")
+
+# Raster de uso e cobertura do solo ----
+
+## Baixar ----
+
+raster_uso <- purrr::map(
+  1985:2025,
+  purrr::in_parallel(
+
+    \(periodo){
+
+      tryCatch({
+
+        terra::rast(paste0(
+          "https://storage.googleapis.com/mapbiomas-public/initiatives/brasil/collection_10/lulc/coverage/brazil_coverage_",
+          periodo,
+          ".tif")) |>
+          terra::crop(apa) |>
+          terra::mask(apa)
+
+      },
+      error = \(e) {
+
+        message("Erro no ano ", periodo, ": ", e$message) |>
+          crayon::red()
+        NULL
+
+      })
+
+    }
+
+  ),
+  .progress = TRUE) |>
+  setNames(1985:2025 |> as.character())
+
