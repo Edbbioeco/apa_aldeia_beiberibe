adc544195eaa9b8ee6c884f15ebddbb26cd04909
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-22 20:04:39 -0300

    Ajustar os limites do raster no mapa e dar valores dos eixos em Norte e Leste

--- diversidade_paisagem.R
+++ diversidade_paisagem.R
@@ -125,6 +125,8 @@ purrr::pmap(
           guide = guide_legend(title.position = "top",
                                title.hjust = 0.5)) +
         labs(title = ano) +
+        coord_sf(expand = FALSE,
+                 label_graticule = "NSWE")
         theme_bw() +
         theme(axis.text = element_text(color = "black", size = 20),
               legend.text = element_text(color = "black", size = 20),
