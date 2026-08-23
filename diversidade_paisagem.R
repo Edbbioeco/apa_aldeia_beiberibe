0f5b7c53d59dd51b4cbb2550141f120dd1c6a3b5
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-23 17:22:29 -0300

    Alterar o nome da variável de diversidade

--- diversidade_paisagem.R
+++ diversidade_paisagem.R
@@ -207,20 +207,20 @@ div_apa <- purrr::imap_dfr(
                       as.numeric()),
   .progress = TRUE) |>
   dplyr::select(6:7) |>
-  dplyr::rename("Diversidade (Gini-Simpson)" = 1)
+  dplyr::rename("Diversidade da paisagem (Gini-Simpson)" = 1)
 
 div_apa
 
 ## Gráfico ----
 
 div_apa |>
-  ggplot(aes(Ano, `Diversidade (Gini-Simpson)`)) +
+  ggplot(aes(Ano, `Diversidade da paisagem (Gini-Simpson)`)) +
   geom_line(linewidth = 1) +
   geom_vline(xintercept = 2010, linewidth = 1, color = "darkgreen") +
   geom_label(data = tibble(Ano = 2010,
-                           `Diversidade (Gini-Simpson)` = 0.64),
+                           `Diversidade da paisagem (Gini-Simpson)` = 0.64),
              aes(Ano,
-                 `Diversidade (Gini-Simpson)`,
+                 `Diversidade da paisagem (Gini-Simpson)`,
                  label = "Criação da APA Aldeia Beiberibe"),
            color = "black",
            fill = "green",
